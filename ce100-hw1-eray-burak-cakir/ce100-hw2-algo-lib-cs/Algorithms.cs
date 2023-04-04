7ac007896ae305f4d803c0cbd1c241950a69ff79
cakirerayburak <cakirerayburak@gmail.com>
2023-04-04 17:55:03 +0300

    Project updated and bugs are fixed.

--- ce100-hw1-eray-burak-cakir/ce100-hw2-algo-lib-cs/Algorithms.cs
+++ ce100-hw1-eray-burak-cakir/ce100-hw2-algo-lib-cs/Algorithms.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/*
+/**
  
  * @author Eray Burak ÇAKIR and Süleyman Mert ALMALI
  
  */
 namespace ce100_hw2_algo_lib_cs
 {
-     
+
 
 
     public class HeapSortAlgorithm
@@ -24,7 +24,7 @@ namespace ce100_hw2_algo_lib_cs
         /// <param name="outputArray">The sorted output array.</param>
         /// <returns>Returns 0 on success or -1 on failure.</returns>
 
-        public static int HeapSort(int[] inputArray, out int[] outputArray)
+        public static int HeapSort(int[] inputArray, out int[] outputArray, bool enableDebug = false)
         {
             outputArray = null;
 
@@ -37,13 +37,23 @@ namespace ce100_hw2_algo_lib_cs
             Array.Copy(inputArray, outputArray, inputArray.Length);
 
             int n = outputArray.Length;
-
+            int finalScore = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                finalScore += inputArray[i] * Convert.ToInt32(Math.Pow(10, inputArray.Length - i - 1));
+            }
+            if (enableDebug == true) { Console.WriteLine("Iteration1: " + finalScore); }
             // Max heap oluşturma işlemi
             for (int i = n / 2 - 1; i >= 0; i--)
             {
                 Heapify(outputArray, n, i);
             }
-
+            int finalScore1 = 0;
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                finalScore1 += outputArray[i] * Convert.ToInt32(Math.Pow(10, outputArray.Length - i - 1));
+            }
+            if (enableDebug == true) { Console.WriteLine("Iteration2: " + finalScore1); }
             // Max heap üzerinde dolaşarak sıralama işlemi
             for (int i = n - 1; i > 0; i--)
             {
@@ -55,7 +65,12 @@ namespace ce100_hw2_algo_lib_cs
                 // Sondaki eleman haricinde kalan elemanlar için max heap oluşturma
                 Heapify(outputArray, i, 0);
             }
-
+            int finalScore2 = 0;
+            for (int i = 0; i < outputArray.Length; i++)
+            {
+                finalScore2 += outputArray[i] * Convert.ToInt32(Math.Pow(10, outputArray.Length - i - 1));
+            }
+            if (enableDebug == true) { Console.WriteLine("Iteration3: " + finalScore2); }
             return 0; // İşlem başarılı olduğunda 0 döndürüyoruz.
         }
 
@@ -100,6 +115,88 @@ namespace ce100_hw2_algo_lib_cs
     }
 
 
+    /**
+     * @brief This class provides a method for multiplying matrices using a dynamic programming approach.
+     */
+
+    public class MemorizedRecursiveMultiplicationAlgorithm
+    {
+
+        /**
+     * @brief Computes the minimum number of scalar multiplications needed to multiply the given matrices.
+
+     * This method uses a recursive approach with memoization to compute the minimum number of scalar
+
+     * multiplications needed to multiply the given matrices. The results of subproblems are stored in
+     
+     * a two-dimensional array for efficiency. If the `enableDebug` parameter is set to `true`, the
+     * values of the memoization table will be printed to the console during execution.
+      
+     * @param p An array containing the dimensions of the matrices to be multiplied.
+     * @param enableDebug If `true`, print the values of the memoization table to the console.
+     * @return The minimum number of scalar multiplications needed to multiply the given matrices.
+     */
+
+
+
+
+        public static int MCMMRM(int[] p, bool enableDebug = false)
+        {
+            int n = p.Length;
+            int[,] m = new int[n, n];
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < n; j++)
+                {
+                    m[i, j] = -1;
+                }
+            }
+
+            int LookupChain(int i, int j)
+            {
+                if (m[i, j] != -1)
+                {
+                    return m[i, j];
+                }
+
+                if (i == j)
+                {
+                    m[i, j] = 0;
+                }
+                else
+                {
+                    for (int k = i; k < j; k++)
+                    {
+                        int q = LookupChain(i, k) + LookupChain(k + 1, j) + p[i - 1] * p[k] * p[j];
+
+                        if (m[i, j] == -1 || q < m[i, j])
+                        {
+                            m[i, j] = q;
+                        }
+                    }
+                }
+
+                if (enableDebug)
+                {
+                    Console.WriteLine($"m[{i}, {j}] = {m[i, j]}");
+                }
+
+                return m[i, j];
+            };
+
+            return LookupChain(1, n - 1);
+        }
+    }
+
+
+
+
+
+
+
+
+
     public class MatrixChainMultiplicationAlgorithm
     {
         /**
@@ -118,37 +215,53 @@ namespace ce100_hw2_algo_lib_cs
 
         */
 
+
+
         private readonly static int[,] dp2 = new int[100, 100];
 
         // Function for matrix chain multiplication
-        static int matrixChainMemoised(int[] p, int i, int j)
+        public static int MatrixChainOrder_DP(int[] p, int n, bool enableDebug = false)
         {
-            if (i == j)
+            int[,] m = new int[n, n];
+
+            for (int i = 1; i < n; i++)
             {
-                return 0;
+                m[i, i] = 0;
             }
-            if (dp2[i, j] != -1)
+
+            for (int L = 2; L < n; L++)
             {
-                return dp2[i, j];
+                for (int i = 1; i < n - L + 1; i++)
+                {
+                    int j = i + L - 1;
+                    m[i, j] = int.MaxValue;
+
+                    for (int k = i; k <= j - 1; k++)
+                    {
+                        int q = m[i, k] + m[k + 1, j] + p[i - 1] * p[k] * p[j];
+                        if (q < m[i, j])
+                        {
+                            m[i, j] = q;
+                        }
+                    }
+                }
             }
-            dp2[i, j] = Int32.MaxValue;
-            for (int k = i; k < j; k++)
+
+            if (enableDebug)
             {
-                dp2[i, j] = Math.Min(
-                  dp2[i, j], matrixChainMemoised(p, i, k)
-                  + matrixChainMemoised(p, k + 1, j)
-                  + p[i - 1] * p[k] * p[j]);
+                Console.WriteLine("Dynamic Programming matrix:");
+                for (int i = 1; i < n; i++)
+                {
+                    for (int j = 1; j < n; j++)
+                    {
+                        Console.Write("{0,7}", m[i, j]);
+                    }
+                    Console.WriteLine();
+                }
             }
-            return dp2[i, j];
+
+            return m[1, n - 1];
         }
-
-        public static int MatrixChainMultiplication_DP(int[] p, int n)
-        {
-            int i = 1, j = n - 1;
-            return matrixChainMemoised(p, i, j);
-        }
-
-
 
 
 
@@ -211,261 +324,127 @@ namespace ce100_hw2_algo_lib_cs
 
 
 
-        public class LongestCommonSubsequence
-        {
-            /**
+    public class LongestCommonSubsequence
+    {
+        /**
 
-            Computes the length of the Longest Common Subsequence (LCS) between two strings.
+        Computes the length of the Longest Common Subsequence (LCS) between two strings.
 
-            @param A The first string.
+        @param A The first string.
 
-            @param B The second string.
+        @param B The second string.
 
-            @return The length of the LCS.
-            */
-
-            public static int LCS(string A, string B)
-            {
-                int m = A.Length;
-                int n = B.Length;
-
-                // create table to store LCS length
-                int[,] dp = new int[m + 1, n + 1];
-
-                // fill table with LCS length values
-                for (int i = 1; i <= m; i++)
-                {
-                    for (int j = 1; j <= n; j++)
-                    {
-                        if (A[i - 1] == B[j - 1])
-                        {
-                            dp[i, j] = dp[i - 1, j - 1] + 1;
-                        }
-                        else
-                        {
-                            dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
-                        }
-                    }
-                }
-
-                // LCS length is in the last cell of the table
-                return dp[m, n];
-            }
-
-
-
-        }
-
-
-            /**
-
-        @brief This method solves the 0-1 knapsack problem using dynamic programming.
-
-        @param values An array containing the values of the items.
-
-        @param weight An array containing the weights of the items.
-
-        @param maxBenefit The maximum benefit that can be obtained from the knapsack.
-
-        @return The maximum value that can be obtained from the knapsack.
-
-        @details This method solves the 0-1 knapsack problem using dynamic programming.
-
-        The problem is defined as follows: given a set of items, each with a weight and a value,
-
-        determine the number of each item to include in a collection so that the total weight
-
-        is less than or equal to a given limit and the total value is as large as possible.
-
-        The method returns the maximum value that can be obtained from the knapsack.
-
+        @return The length of the LCS.
         */
 
+        public static int LCS(string A, string B, bool enableDebug = false)
+        {
+            int m = A.Length;
+            int n = B.Length;
 
-            public class KnapsackAlgorithm
+            // create table to store LCS length
+            int[,] dp = new int[m + 1, n + 1];
+
+            // fill table with LCS length values
+            for (int i = 1; i <= m; i++)
             {
-
-
-                public static int Knapsack01(int[] values, int[] weigth, int maxBenefit)
+                for (int j = 1; j <= n; j++)
                 {
-                    int N = values.Length;
-                    int[,] m = new int[N + 1, maxBenefit + 1];
-
-                    for (int c = 0; c <= maxBenefit; c++)
+                    if (A[i - 1] == B[j - 1])
                     {
-                        m[0, c] = 0;
-                    }
-
-                    for (int i = 1; i <= N; i++)
-                    {
-                        for (int c = 0; c <= maxBenefit; c++)
-                        {
-                            if (weigth[i - 1] <= c)
-                            {
-                                m[i, c] = Math.Max(m[i - 1, c], values[i - 1] + m[i - 1, c - weigth[i - 1]]);
-                            }
-                            else
-                            {
-                                m[i, c] = m[i - 1, c];
-                            }
-                        }
-                    }
-
-                    if (m[N, maxBenefit] == int.MinValue)
-                    {
-                        return -1;
+                        dp[i, j] = dp[i - 1, j - 1] + 1;
+                        if (enableDebug == true) { Console.WriteLine("Iteration: " + dp[i, j] + " changed with " + dp[i - 1, j - 1] + 1); }
                     }
                     else
                     {
-                        return m[N, maxBenefit];
+                        dp[i, j] = Math.Max(dp[i - 1, j], dp[i, j - 1]);
+                        if (enableDebug == true) { Console.WriteLine("Iteration: " + dp[i, j] + " changed with " + Math.Max(dp[i - 1, j], dp[i, j - 1])); }
                     }
                 }
+            }
 
-
+            // LCS length is in the last cell of the table
+            return dp[m, n];
+        }
 
 
 
     }
 
 
+                /**
+
+            @brief This method solves the 0-1 knapsack problem using dynamic programming.
+
+            @param values An array containing the values of the items.
+
+            @param weight An array containing the weights of the items.
+
+            @param maxBenefit The maximum benefit that can be obtained from the knapsack.
+
+            @return The maximum value that can be obtained from the knapsack.
+
+            @details This method solves the 0-1 knapsack problem using dynamic programming.
+
+            The problem is defined as follows: given a set of items, each with a weight and a value,
+
+            determine the number of each item to include in a collection so that the total weight
+
+            is less than or equal to a given limit and the total value is as large as possible.
+
+            The method returns the maximum value that can be obtained from the knapsack.
+
+            */
+
+
+    public class KnapsackAlgorithm
+    {
+
+
+        public static int Knapsack01(int[] values, int[] weigth, int maxBenefit, bool enableDebug = false)
+        {
+            int N = values.Length;
+            int[,] m = new int[N + 1, maxBenefit + 1];
+
+            for (int c = 0; c <= maxBenefit; c++)
+            {
+                m[0, c] = 0;
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int c = 0; c <= maxBenefit; c++)
+                {
+                    if (weigth[i - 1] <= c)
+                    {
+                        int a = Math.Max(m[i - 1, c], values[i - 1] + m[i - 1, c - weigth[i - 1]]);
+                        m[i, c] = a;
+                        if (enableDebug == true) { Console.WriteLine("Iteration: " + m[i, c] + " changed with " + a); }
+                    }
+                    else
+                    {
+                        m[i, c] = m[i - 1, c];
+                        if (enableDebug == true) { Console.WriteLine("Iteration: " + m[i, c] + " changed with " + m[i - 1, c]); }
+                    }
+                }
+            }
+
+            if (m[N, maxBenefit] == int.MinValue)
+            {
+                return -1;
+            }
+            else
+            {
+                return m[N, maxBenefit];
+            }
+        }
 
 
 
 
 
-
-
-
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-//public void KnapsackDyProg(int[] W, int[] V, int M, int n)
-//{
-//    int[,] B = new int[n + 1, M + 1];
-
-//    for (int i = 0; i <= n; i++)
-//        for (int j = 0; j <= M; j++)
-//        {
-//            B[i, j] = 0;
-//        }
-
-//    for (int i = 1; i <= n; i++)
-//    {
-//        for (int j = 0; j <= M; j++)
-//        {
-//            B[i, j] = B[i - 1, j];
-
-//            if ((j >= W[i - 1]) && (B[i, j] < B[i - 1, j - W[i - 1]] + V[i - 1]))
-//            {
-//                B[i, j] = B[i - 1, j - W[i - 1]] + V[i - 1];
-//            }
-
-//            Console.Write(B[i, j] + " ");
-//        }
-//        Console.Write("\n");
-//    }
-
-//    Console.WriteLine("Max Value:\t" + B[n, M]);
-
-//    Console.WriteLine("Selected Packs: ");
-
-//    while (n != 0)
-//    {
-//        if (B[n, M] != B[n - 1, M])
-//        {
-//            Console.WriteLine("\tPackage " + n + " with W = " + W[n - 1] + " and Value = " + V[n - 1]);
-
-//            M = M - W[n - 1];
-//        }
-
-//        n--;
-//    }
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//public int mcmrem(int[] matrixDimensionArray, ref string matrixOrder, ref int operationCount)
-//    {
-//        int n = matrixDimensionArray.Length - 1;
-//        int[,] s = new int[n + 1, n + 1];
-//        int[,] m = new int[n + 1, n + 1];
-
-//        for (int i = 1; i <= n; i++)
-//        {
-//            m[i, i] = 0;
-//        }
-
-//        MatrixChainOrder(matrixDimensionArray, m, s, 1, n);
-
-//        matrixOrder = PrintOptimalParens(s, 1, n);
-//        operationCount = m[1, n];
-
-//        return 0;
-//    }
-
-//    private void MatrixChainOrder(int[] p, int[,] m, int[,] s, int i, int j)
-//    {
-//        if (i == j)
-//        {
-//            return;
-//        }
-
-//        m[i, j] = int.MaxValue;
-
-//        for (int k = i; k < j; k++)
-//        {
-//            int q = m[i, k] + m[k + 1, j] + p[i - 1] * p[k] * p[j];
-
-//            if (q < m[i, j])
-//            {
-//                m[i, j] = q;
-//                s[i, j] = k;
-//            }
-//        }
-
-//        MatrixChainOrder(p, m, s, i, s[i, j]);
-//        MatrixChainOrder(p, m, s, s[i, j] + 1, j);
-//    }
-
-//    private string PrintOptimalParens(int[,] s, int i, int j)
-//    {
-//        if (i == j)
-//        {
-//            return "A" + i;
-//        }
-//        else
-//        {
-//            return "(" + PrintOptimalParens(s, i, s[i, j]) + PrintOptimalParens(s, s[i, j] + 1, j) + ")";
-//...
-//        }
-//    }
-
-
-
-
 
 
 
--- ce100-hw1-eray-burak-cakir/ce100-hw2-algo-lib-csTests/UNITTEST.cs
+++ ce100-hw1-eray-burak-cakir/ce100-hw2-algo-lib-csTests/UNITTEST.cs
@@ -3,6 +3,9 @@ using ce100_hw2_algo_lib_cs;
 using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Security.Policy;
+
 namespace UNIT.Tests
 {
 
@@ -19,7 +22,7 @@ namespace UNIT.Tests
 
 
     [TestClass]
-    public class HEAPSORTTEST : HeapSortAlgorithm
+    public class HeapSortTest : HeapSortAlgorithm
     {
         [TestMethod]
         public void BestCase()
@@ -29,7 +32,7 @@ namespace UNIT.Tests
             int[] expectedOutputArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
             // Act
-            int result = HeapSort(inputArray, out int[] outputArray);
+            int result = HeapSort(inputArray, out int[] outputArray, true);
 
             // Assert
             Assert.AreEqual(0, result);
@@ -44,7 +47,7 @@ namespace UNIT.Tests
             int[] expectedOutputArray = { 1, 2, 3, 4, 5, 6, 6 };
 
             // Act
-            int result = HeapSort(inputArray, out int[] outputArray);
+            int result = HeapSort(inputArray, out int[] outputArray, true);
 
             // Assert
             Assert.AreEqual(0, result);
@@ -59,7 +62,7 @@ namespace UNIT.Tests
             int[] expectedOutputArray = null;
 
             // Act
-            int result = HeapSort(inputArray, out int[] outputArray);
+            int result = HeapSort(inputArray, out int[] outputArray, true);
 
             // Assert
             Assert.AreEqual(-1, result);
@@ -73,56 +76,147 @@ namespace UNIT.Tests
 
     }
 
-
     [TestClass]
-    public class LongestCommonSubsequenceTest : LongestCommonSubsequence
+    public class MemorizedRecursiveMultiplicationTest : MemorizedRecursiveMultiplicationAlgorithm 
     {
-        [TestMethod]
-        public void BestCaseTest()
-        {
-            string A = "ABC";
-            string B = "DEF";
-            int expected = 0;
-            int result = LCS(A, B);
-            Assert.AreEqual(expected, result);
-
-
-        }
 
         [TestMethod]
-        public void AverageCaseTest()
+        public void BestCase()
         {
-            string A = "ABCDEF";
-            string B = "DEFABC";
-            int expected = 3;
-            int result = LCS(A, B);
-            Assert.AreEqual(expected, result);
+            int[] p = { 10, 20, 30, 40, 50, 60, 70 };
+            int expected = 110000;
 
 
-        }
+            int n = p.Length - 1;
+            int[,] m = new int[n, n];
 
-        [TestMethod]
-        public void WorstCaseTest()
-        {
-            // Arrange
-            string A = "ABCDEFGHIJKLMNO";
-            string B = "PQRSTUVWXYZ1234567890";
-            int expected = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    m[i, j] = -1;
+                }
+            }
 
-            // Act
-            int result = LCS(A, B);
+            int result = MCMMRM(p, true);
 
-            // Assert
             Assert.AreEqual(expected, result);
         }
 
 
 
+        [TestMethod]
+        public void AverageCase()
+        {
+            int[] p = { 30, 35, 15, 30, 10, 20, 25 };
+            int expected = 32750;
 
+
+            int n = p.Length - 1;
+            int[,] m = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    m[i, j] = -1;
+                }
+            }
+
+            int result = MCMMRM(p, true);
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void WorstCase()
+        {
+            int[] p = { 10, 20, 30, 40, 30 };
+            int expected = 30000;
+
+
+            int n = p.Length - 1;
+            int[,] m = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    m[i, j] = -1;
+                }
+            }
+
+
+            int result = MCMMRM(p,true);
+
+            Assert.AreEqual(expected, result);
+
+        }
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
+
+        [TestClass]
+        public class LongestCommonSubsequenceTest : LongestCommonSubsequence
+        {
+            [TestMethod]
+            public void BestCaseTest()
+            {
+                string A = "ABC";
+                string B = "DEF";
+                int expected = 0;
+                int result = LCS(A, B, true);
+                Assert.AreEqual(expected, result);
+
+
+            }
+
+            [TestMethod]
+            public void AverageCaseTest()
+            {
+                string A = "ABCDEF";
+                string B = "DEFABC";
+                int expected = 3;
+                int result = LCS(A, B, true);
+                Assert.AreEqual(expected, result);
+
+
+            }
+
+            [TestMethod]
+            public void WorstCaseTest()
+            {
+                // Arrange
+                string A = "ABCDEFGHIJKLMNO";
+                string B = "PQRSTUVWXYZ1234567890";
+                int expected = 0;
+
+                // Act
+                int result = LCS(A, B, true);
+
+                // Assert
+                Assert.AreEqual(expected, result);
+            }
+
+
+
+
+
+
+
+        }
 
 
 
@@ -184,40 +278,34 @@ namespace UNIT.Tests
 
 
 
-        private readonly static int[,] dp2 = new int[100, 100];
         [TestMethod]
-        public void MatrixChainMultiplication_DP_BestCaseTest()
+        public void MatrixChainMultiplication_DP_AverageCase()
         {
-            int[] arr = { 1, 2, 3, 4 };
-            int n = arr.Length;
 
-
-            int result = MatrixChainMultiplication_DP(arr, n);
-            // Assert
-            Assert.AreEqual(30000, result);
-        }
-
-
-
-        [TestMethod]
-        public void MatrixChainMultiplication_DP_WorstCaseTest()
-        {
-            // Arrange
-            int[] p = { 10, 20, 30, 40, 50 };
+            int[] p = new int[] { 10, 20, 30, 40, 30 };
             int n = p.Length;
 
-            for (int i = 0; i < 100; i++)
-            {
-                for (int j = 0; j < 100; j++)
-                {
-                    dp2[i, j] = -1;
-                }
-            }
-            // Act
-            int result = MatrixChainMultiplication_DP(p, n);
 
-            // Assert
+            int result = MatrixChainOrder_DP(p, n, true);
+
+
             Assert.AreEqual(30000, result);
+
+        }
+
+        [TestMethod]
+        public void MatrixChainMultiplication_DP_WorstCase()
+        {
+
+            int[] p = new int[] { 30, 35, 15, 5, 10, 20, 25 };
+            int n = p.Length;
+
+
+            int result = MatrixChainOrder_DP(p, n, true);
+
+
+            Assert.AreEqual(15125, result);
+
         }
     }
 
@@ -227,7 +315,7 @@ namespace UNIT.Tests
 
 
     [TestClass]
-    public class KnapsackProblem : KnapsackAlgorithm
+    public class KnapsackProblemTest : KnapsackAlgorithm
     {
 
         [TestMethod]
@@ -239,11 +327,11 @@ namespace UNIT.Tests
             int maxBenefit = 13;
             int expected = 5;
 
-            int output = Knapsack01(weights, values, maxBenefit);
+            int output = Knapsack01(weights, values, maxBenefit, true);
 
             Assert.AreEqual(expected, output);
 
-            //Assert.AreEqual(output);
+
 
 
 
@@ -260,7 +348,7 @@ namespace UNIT.Tests
 
             int expected = 6;
 
-            int output = Knapsack01(weights, values, maxBenefit);
+            int output = Knapsack01(weights, values, maxBenefit, true);
 
             Assert.AreEqual(expected, output);
 
@@ -276,43 +364,13 @@ namespace UNIT.Tests
 
             int expected = 0;
 
-            int output = Knapsack01(weights, values, maxBenefit);
+            int output = Knapsack01(weights, values, maxBenefit, true);
 
             Assert.AreEqual(expected, output);
 
 
 
         }
-
-
-
-
-
-        //[TestMethod]
-        //public void MCMRecursiveMemorized_ReturnsCorrectOutput()
-        //{
-        //    // Arrange
-        //    int[] matrixDimensions = { 10, 30, 5, 60 };
-        //    int expectedOperationCount = 4500;
-        //    string expectedMatrixOrder = "((A1(A2A3))A4)";
-
-        //    // Act
-        //    string matrixOrder;
-        //    int operationCount;
-        //    MATRIXMULTIPLICATION.MCMRecursiveMemorized(matrixDimensions, out matrixOrder, out operationCount);
-
-        //    // Assert
-        //    Assert.AreEqual(expectedOperationCount, operationCount);
-        //    Assert.AreEqual(expectedMatrixOrder, matrixOrder);
-
-        //}
-
-
-
-
-
-
-
 
 
 
